298d174c260dcd7dc23fcb8c3f1984e50f3b828b
seulayoon <seulaayoon@gmail.com>
2021-11-06 20:15:28 +0900

    완전탐색1.py upload

---
+++ 완전탐색1.py
@@ -0,0 +1,22 @@
+def solution(answers):
+    a = [1, 2, 3, 4, 5]
+    b = [2, 1, 2, 3, 2, 4, 2, 5]
+    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    counts = [0, 0, 0]
+    answer = []
+    for i in range(len(answers)):
+        if answers[i] == a[(i % 5)]:
+            counts[0] += 1
+        if answers[i] == b[(i % 8)]:
+            counts[1] += 1
+        if answers[i] == c[(i % 10)]:
+            counts[2] += 1
+
+    for i in range(3):
+        if counts[i] == max(counts):
+            answer.append(i + 1)
+    answer.sort()
+
+    return answer
+
+print(solution([1,2,3,4,5]))
